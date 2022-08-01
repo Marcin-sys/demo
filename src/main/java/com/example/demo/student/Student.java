7b59f3b1f50c035a19bcb8c43e1ac15a2cdfb1f6
Marcin M <mircin27@gmail.com>
2022-08-01 19:09:27 +0200

    Fixed Typo Added Generator in Student

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,25 @@
 package com.example.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Id
+//    @Column(
+//            name = "id",
+//            updatable = false
+//    )
     private Long id;
     private String name;
     private String email;
@@ -14,13 +31,6 @@ public class Student {
 
     public Student(Long id, String name, String email, LocalDate dob, Integer age) {
         this.id = id;
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-        this.age = age;
-    }
-
-    public Student(String name, String email, LocalDate dob, Integer age) {
         this.name = name;
         this.email = email;
         this.dob = dob;
